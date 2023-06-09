c18334f08ca16bf4662425b379836886d7475882
Maks_Nik_CPP7 <odionyx@yandex.ru>
2023-06-09 13:13:31 +0400

    Initial commit

---
+++ task1_preprocessor_directives/task1_preprocessor_directives.cpp
@@ -0,0 +1,29 @@
+﻿// task1_preprocessor_directives.cpp
+#include <iostream>
+#define add(a,b) a+=b
+#define MODE 0
+#ifndef MODE
+#error <MODE is not initialized>
+#endif
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "ru_RU");
+    int a = 0;
+    int b = 0;
+#if (MODE == 0)
+    std::cout << "Работаю в режиме тренировки..." << std::endl;
+#elif (MODE == 1):
+    std::cout << "Работаю в боевом режиме..." << std::endl;
+    add(a, b);
+    std::cout << "Введите число 1: ";
+    std::cin >> a;
+    std::cout << "Введите число 2: ";
+    std::cin >> b;
+    std::cout << "Результат сложения: " << (add(a, b))<< std::endl;
+#else ((MODE != 0) && (MODE !=1))
+    std::cout << "Неизвестный режим. Завершение работы." << std::endl;
+#endif
+#undef MODE
+    return 0;
+}
